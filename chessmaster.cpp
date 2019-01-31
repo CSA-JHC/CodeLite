13cc95900126aa0c4cb141a287733cbd061fe568
CSA-JHC <1881@chaparralstaracademy.com>
2019-01-31 12:19:33 -0600

    Add files via upload

--- chessmaster.cpp
+++ chessmaster.cpp
@@ -3,7 +3,36 @@ using namespace std;
 
 int main()
 {
+	int pennies=0;
+	int value=1;
+	int squares=0;
 	
+	while (pennies<1000)
+	{
+		value*=2;
+		pennies+=value;
+		squares+=1;
+	}
+	
+	cout<<squares<<" square(s) are required to earn 1000 pennies \n";
+	
+	while (pennies<1000000)
+	{
+		value*=2;
+		pennies+=value;
+		squares+=1;
+	}
+	
+	cout<<squares<<" square(s) are required to earn 1000000 pennies \n";
+	
+	while (pennies<1000000000)
+	{
+		value*=2;
+		pennies+=value;
+		squares+=1;
+	}
+	
+	cout<<squares<<" square(s) are required to earn 1000000000 pennies \n";
 	
 	return 0;
 }
