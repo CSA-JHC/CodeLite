b0357d8fb927edf87bcd9cad862c50ba9a9f5948
CSA-JHC <1881@chaparralstaracademy.com>
2019-02-04 10:47:21 -0600

    Add files via upload

--- chessmaster.cpp
+++ chessmaster.cpp
@@ -1,12 +1,17 @@
 #include <iostream>
 using namespace std;
 
+//JESSICA CHIU
+
 int main()
 {
+	//identify variables
 	int pennies=0;
 	int value=1;
 	int squares=0;
 	
+	//determine the amount of squares on a chessboard until it reaches 10000
+	//multiply by 2 and add it to value until it reaches required amount (while loop)
 	while (pennies<1000)
 	{
 		value*=2;
@@ -14,8 +19,9 @@ int main()
 		squares+=1;
 	}
 	
-	cout<<squares<<" square(s) are required to earn 1000 pennies \n";
+	cout<<squares<<" square(s) are required to earn 1000 pennies \n"; //tell the user
 	
+	//do the same thing for 1000000 and 1000000000
 	while (pennies<1000000)
 	{
 		value*=2;
--- game.cpp
+++ game.cpp
@@ -1,12 +1,107 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
+
+//JESSICA CHIU
 
 int main()
 {
-	string hand;
-	int value=0;
+	//identify variables
+	int number;
+	char position;
+	string aiposition;
+	unsigned seed;
 	
-	position=rand()%3+1;
+	//ask the user for input
+	cout<<"Rock (r), Paper (p) or Scissors (s)? Please enter the corresponding letter: ";
+	cin>>position;
+	
+	//set up random function to find position of the oponent
+	seed=time(0);
+	srand(seed);
+	
+	number=rand()%3+1;
+	
+	//based off of random, let the user know what they decided to play
+	switch (number)
+	{
+		case 1:
+		{
+			aiposition="r";
+			cout<<"Your oponent chose Rock \n";
+		}
+		break;
+		
+		case 2:
+		{
+			aiposition="p";
+			cout<<"Your oponent chose Paper \n";
+		}
+		break;
+		
+		case 3:
+		{
+			aiposition="s";
+			cout<<"Your oponent chose Scissors \n";
+		}
+		break;
+	}
+	
+	//based off of users input, determine whether they lost, won, or tied
+	switch (position)
+	{
+		case 'r':
+		{
+			if (aiposition=="r")
+			{
+				cout<<"You tied!\n";
+			}
+			else if (aiposition=="p")
+			{
+				cout<<"You lost!\n";
+			}
+			else if (aiposition=="s")
+			{
+				cout<<"You won!\n";
+			}
+		}
+		break;
+		
+		case 'p':
+		{
+			if (aiposition=="r")
+			{
+				cout<<"You won!\n";
+			}
+			else if (aiposition=="p")
+			{
+				cout<<"You tied!\n";
+			}
+			else if (aiposition=="s")
+			{
+				cout<<"You lost!\n";
+			}
+		}
+		break;
+		
+		case 's':
+		{
+			if (aiposition=="r")
+			{
+				cout<<"You lost!\n";
+			}
+			else if (aiposition=="p")
+			{
+				cout<<"You won!\n";
+			}
+			else if (aiposition=="s")
+			{
+				cout<<"You tied!\n";
+			}
+		}
+		break;
+	}
 	
 	return 0;
 }
