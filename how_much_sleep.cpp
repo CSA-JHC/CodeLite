b9912f36d3343e3a45affdbc19c73cd2d5474065
CSA-JHC <1881@chaparralstaracademy.com>
2019-02-16 12:37:30 -0600

    Add files via upload

--- how_much_sleep.cpp
+++ how_much_sleep.cpp
@@ -2,38 +2,29 @@
 #include <cmath>
 using namespace std;
 
-/* Objective: using user inputs, calculate the total amount of possible sleep
- * this person can get in a week.
- * 
- * Input needed:
- * max hrs of school per day
- * extracurricular activities (total? or by activity?)
- * practice of activity?
- * biological needs (food, bathroom)
- * travel (to and from activites)
- * leisure time
- * hw (what if you do some at school?)
-*/
-
-/*	when to use a function: whenever you find yourself cutting and pasting code over and over.
- * why use a function:
- * 	abstraction - assume and implementation works, only deal with interface
- * 	modularity - Legos.. building a program out of descrete chunks
- * 		gain the ability to fix smaller chunks instead of checking every line of code
- */
+//JESSICA CHIU
 
 void test_time(float activity, int limit) //test if amount of time given is valid
 	{
-		char yesno;
-		
-		while (activity>limit)
-		{
-			cout<<"That's odd but ok...\n";
-		}
+        if (activity>limit)
+        {
+            cout<<"That doesn't seem right.. try again..\n";
+            cout<<'\n';
+        }
+	}
+
+void test_amount(float activity, int limit) //test if amount of days for extra curriculars given is valid
+	{
+        if (activity>limit)
+        {
+            cout<<"That doesn't seem right.. try again..\n";
+            cout<<'\n';
+        }
 	}
 
 int main()
 {
+    //define variables
 	const int hours_week=168;
 	float sleephrs=168;
 	char timeunit='m';
@@ -46,32 +37,34 @@ int main()
 	float freehrs=0;
 	
 	char start;
-
-
+    
+    //explain the proram
 	cout<<"This program will determine how much sleep you will get in a week!\n\n";
 	
+    //calculate the amount of hours it takes to eat dinner every week
 	int dinner_limit=2;
-	
-	do {
-		cout << "How much time do you spend eating dinner a day? ";
+
+	do { //keep asking if the time seems unreasonable
+        cout << "How much time do you spend eating dinner a day? ";
 	    cin >> dinnerhrs;
-	    cout << "Was that in minutes (m) or hours (h)? \n";
+	    cout << "Was that in minutes (m) or hours (h)? ";
 	    cin >> timeunit;
 	    if(timeunit == 'm') {
-	    dinnerhrs = dinnerhrs / 60; // convert minutes to hours
+	    dinnerhrs = dinnerhrs / 60; // convert minutes to hours if time given is in minutes
 	    }
-	    test_time(dinnerhrs,dinner_limit);
+	    test_time(dinnerhrs,dinner_limit); //test amount given
 	} while(dinnerhrs>dinner_limit);
-	dinnerhrs=dinnerhrs*7;
-	sleephrs-=dinnerhrs;
-	//cout<<sleephrs;
+	dinnerhrs=dinnerhrs*7; //total amount per week
+	sleephrs-=dinnerhrs; //subtract from total amount of hours
+	cout<<'\n';
 	
-	int school_limit=45;
+    //calculate the amount of hours they are at school
+	int school_limit=9;
 	
 	do {
 		cout << "How much time do you spend at school a day? ";
 		cin >> schoolhrs;
-		cout<<"Was that in minutes (m) or hours (h)? \n";
+		cout<<"Was that in minutes (m) or hours (h)? ";
 		cin>>timeunit;
 		if(timeunit=='m'){
 		schoolhrs=schoolhrs/60;
@@ -80,34 +73,37 @@ int main()
 	} while(schoolhrs>school_limit);
 	schoolhrs = schoolhrs * 5.0;
 	sleephrs-=schoolhrs;
-	//cout<<sleephrs;
+	cout<<'\n';
 	
-	int extra_limit=56;
+    //calculate the amount of hours spent on extracurriculars
+	int extra_limit=12;
 	float amount=0;
+    int amount_limit=7;
 	
 	do {
 		cout << "How much time do you spend doing extracurriculars a day? ";
 		cin >> extrahrs;
 		cout<<"Was that in minutes (m) or hours (h)? ";
 		cin>>timeunit;
-		cout<<"How many times a week? \n";
+		cout<<"How many times a week? ";
 		cin>>amount;
 		if(timeunit=='m'){
 		extrahrs=extrahrs/60;
-		extrahrs = extrahrs * amount;
 		}
 		test_time(extrahrs,extra_limit);
-	} while(extrahrs>extra_limit);
+        test_amount(amount,amount_limit); //test amount of days given
+	} while((extrahrs>extra_limit)||(amount>amount_limit));
 	extrahrs=extrahrs*amount;
 	sleephrs-=extrahrs;
-	//cout<<sleephrs;
+	cout<<'\n';
 	
+    //calculate amount of time spent in the bathroom
 	int bathroom_limit=5;
 	
 	do {
 		cout << "How much time do you spend in the bathroom a day? ";
 		cin >> bathroomhrs;
-		cout<<"Was that in minutes (m) or hours (h)? \n";
+		cout<<"Was that in minutes (m) or hours (h)? ";
 		cin>>timeunit;
 		if(timeunit=='m'){
 		bathroomhrs=bathroomhrs/60;
@@ -116,14 +112,15 @@ int main()
 	} while(bathroomhrs>bathroom_limit);
 	bathroomhrs=bathroomhrs*7;
 	sleephrs-=bathroomhrs;
-	//cout<<sleephrs;
+	cout<<'\n';
 	
+    //calculate the amount of time spent traveling/driving
 	int travel_limit=24;
 	
 	do {
 		cout << "How much time do you spend traveling a day? ";
 		cin >> travelhrs;
-		cout<<"Was that in minutes (m) or hours (h)? \n";
+		cout<<"Was that in minutes (m) or hours (h)? ";
 		cin>>timeunit;
 		if(timeunit=='m'){
 		travelhrs=travelhrs/60;
@@ -132,14 +129,15 @@ int main()
 	} while(travelhrs>travel_limit);
 	travelhrs = travelhrs * 7.0;
 	sleephrs-=travelhrs;
-	//cout<<sleephrs;
+	cout<<'\n';
 	
+    //calculate the amount of time spent on free time
 	int free_limit=12;
 	
 	do {
 		cout << "How much time do you spend having free time a day? ";
 		cin >> freehrs;
-		cout<<"Was that in minutes (m) or hours (h)? \n";
+		cout<<"Was that in minutes (m) or hours (h)? ";
 		cin>>timeunit;
 		if(timeunit=='m'){
 		freehrs=freehrs/60;
@@ -148,8 +146,9 @@ int main()
 	} while(freehrs>free_limit);
 	freehrs = freehrs * 7.0;
 	sleephrs-=freehrs;
-	//cout<<sleephrs;
+	cout<<'\n';
 	
+    //print how much sleep they have left in the week
 	cout<<"You have "<<sleephrs<<" hours left in the week for sleeping.\n";
 	
 	return 0;
