5df3e63a957e6b5894a4d718793493bf15e267db
CSA-JHC <1881@chaparralstaracademy.com>
2019-04-08 11:07:52 -0500

    Add files via upload

--- teachsupp.cpp
+++ teachsupp.cpp
@@ -4,20 +4,90 @@ using namespace std;
 
 //JESSICA CHIU
 
-//class Teacher{
-//private:
-//	string teachername;
-//	string prefix;
-//	vector<string> supplies{};
-//	
-//public:
-//	void print_info{
-//		cout<<"**"<<teachername<<"'s school supply list**";
-//	}
-//	
-//	
-//}
+/*vector<string> genList={"plastic folders", "glue sticks","pack of colored pencils","pack of crayons","composition book"}
+ * genList[0] //first item in the genList vector
+ * 
+ * vector<string> t_name={"Prater","Threatt","Shafer"}
+ *
+ * class has xyz (private member variables)
+ * do things to xyz (public member variables
+ * 
+ * 
+ * class _____
+ * {
+ * private:
+ * supplies
+ * name
+ * 
+ * public:
+ * string getSupplies(int num)
+ * {
+ * supplies=gentList[num]
+ * return supplies
+ * }
+ * string getName(num index for vector)
+ * {
+ * teach=t_name[num]
+ * return teach
+ * }
+ * string getOutput()
+ * {}
+ * }
+ * 
+ * int main()
+ * {
+ * string getOutput(object.getSupplies,object.getName)
+ * cout<<object.getSupplies(num)
+ * cout<<object.getName(num)
+ * }
+ * 
+ */
 
+class Supplies
+{
+private:
+	string supplies1, supplies2, supplies3;
+	string name1;
+public:
+	void getSupplies(int a, int b, int c)
+	{
+		vector<string> genList={"plastic folders", "glue sticks","pack of colored pencils","pack of crayons","composition book"};
+		supplies1=genList[a];
+		supplies2=genList[b];
+		supplies3=genList[c];
+	}
+	void getName(int x)
+	{
+		vector<string> t_name={"Prater","Threatt","Shafer"};
+		name1=t_name[x];
+	}
+	void format(string name, string supp1, string supp2, string supp3)
+	{
+		cout<<"**"<<name1<<"'s School Supplies List"<<"**"<<'\n';
+		cout<<supplies1<<'\n'<<supplies2<<'\n'<<supplies3<<'\n';
+	}
+	
+};
+
+int main()
+{
+	string asterisk="**";
+	string ssl="School Supply List";
+	
+	Supplies teachname, supp;
+	teachname=teachname.getName(0);
+	supp=supp.getSupplies(0,1,3);
+	
+	teachname=teachname.getName(1);
+	supp=supp.getSupplies(0,2,4);
+	
+	teachname=teachname.getName(2);
+	
+	
+	return 0;
+}
+
+/*
 int main()
 {
 	//boiler plate text
@@ -35,25 +105,23 @@ int main()
 	string cp="pack of colored pencils";
 	
 	//prefix
-	vector<string> prefix={"Mr. ","Mrs. ","Miss ","Ms. ","Dr. "};
+	//vector<string> prefix={"Mr. ","Mrs. ","Miss ","Ms. ","Dr. "};
 	
-	cout<<asterisk<<prefix[2]<<kinder<<"'s "<<ssl<<asterisk<<"\n";
+	cout<<asterisk<<"Ms. "<<kinder<<"'s "<<ssl<<asterisk<<"\n";
 	cout<<pf<<'\n'
 		<<gs<<'\n'
 		<<"packs of crayons \n"
 		<<'\n';
 	
-	cout<<asterisk<<prefix[1]<<socialstudies<<"'s "<<ssl<<asterisk<<"\n";
+	cout<<asterisk<<"Mrs. "<<socialstudies<<"'s "<<ssl<<asterisk<<"\n";
 	cout<<pf<<'\n'
 		<<cp<<'\n'
 		<<"composition book \n"
 		<<'\n';
 	
-	cout<<asterisk<<prefix[0]<<math_science<<"'s "<<ssl<<asterisk<<'\n';
+	cout<<asterisk<<"Mr. "<<math_science<<"'s "<<ssl<<asterisk<<'\n';
 	cout<<gs<<'\n'
 		<<cp<<'\n'
 		<<"ruler \n"
 		<<'\n';
-	
-	return 0;
-}
+*/
